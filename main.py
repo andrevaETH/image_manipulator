d4111d43bc1d272d718e46dd744a95936ab44ba6
vanandre <vanoncini@hotmail.com>
2018-11-11 13:36:22 +0100

    Resizing

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import skimage.io
 import skimage.filters
 import skimage.color
 import skimage.feature
+import numpy as np
 from PIL import Image
 
 
@@ -20,13 +21,25 @@ class ImageReader:
         Constructor
         """
         # - Parameters -
-        self.file_path = "/Users/andrevanoncini/Desktop/Papi_Lokis/Test"
-        self.new_file_path = "/Users/andrevanoncini/Desktop/Papi_Lokis/Test/adjusted"
+        self.file_path = "/Users/andrevanoncini/Desktop/Papi_Lokis"
+        self.new_file_path = "/Users/andrevanoncini/Desktop/Papi_Lokis/adjusted"
         self.aspect_ratio = 3
 
         # - These are the required actions -
         list_paths = self.collect_all_image_files(self.file_path)
         self.process_all_images(list_paths)
+
+    def pil_2_numpy(self, pil_image):
+        """
+        Converts a PIL object to a numpy array
+        """
+        return np.array(pil_image)
+
+    def numpy_2_pil(self, numpy_array):
+        """
+        Converts a numpy array back to a PIL object
+        """
+        pass
 
     def collect_all_image_files(self, file_path):
         """
@@ -75,6 +88,15 @@ class ImageReader:
         view_img = skimage.viewer.ImageViewer(filter_img)
         view_img.show()
 
+    def filter_stack(self, pil_image):
+        """
+        All operations which are done on the image
+        """
+        np_image = self.pil_2_numpy(pil_image)
+
+        gray_im = skimage.color.rgb2gray(np_image)
+        sobel_im = skimage.filters.sobel(gray_im)
+        
     def process_image(self, image_path, image_path_short):
         """
         Method to load an image into RAM and then process it
@@ -86,14 +108,17 @@ class ImageReader:
         im_path = Image.open(image_path)
 
         # - Store dimensions of image and crop -
+        im_path = self.resize_image(im_path)
         im_width = im_path.size[0]
         im_height = im_path.size[1]
+        new_im_height = im_height
+        curr_img = im_path
 
-        new_im_height = im_height / self.aspect_ratio
-        crop_height_zero = (im_height - new_im_height) / 2
+        #new_im_height = im_height / self.aspect_ratio
+        #crop_height_zero = (im_height - new_im_height) / 2
 
-        im_path = im_path.crop((0, crop_height_zero, im_width, crop_height_zero + new_im_height))
-        curr_img = im_path.load()
+        #im_path = im_path.crop((0, crop_height_zero, im_width, crop_height_zero + new_im_height))
+        #curr_img = im_path.load()
 
         # - Replace all completely white pixels with black -
         for x in range(im_width):
@@ -115,6 +140,30 @@ class ImageReader:
         :param filter_kernel:
         :return:
         """
+        np_image = self.pil_2_numpy(pixel_image)
+
+        [width, height] = np_image.shape
+        [filter_width, filter_height] = filter_kernel.shape
+        np_row = np_col = 0
+        while np_row < height:
+            np_col = 0
+            for np_col in range(0, width):
+                if(np_col - filter_width < 0 or np_col + filter_width > width):
+                    # - Case if we are at the border of the image -
+                    pass
+
+    def resize_image(self, pixel_image):
+        """
+        Resizes the image to the required dimensions
+        :param pixel_image:
+        :return:
+        """
+
+        small_image = pixel_image.resize((150, 50), Image.LANCZOS)
+
+        return small_image
+
+
 
 
 def main():
